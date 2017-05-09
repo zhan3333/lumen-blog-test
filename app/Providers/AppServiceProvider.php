7f22651946d9b6162fdc506ad9652d2a6e93d9d9
zhan <390961827@qq.com>
2017-05-09 23:21:04 +0800

    删除Student model 增加 ide 工具

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
     }
 }
